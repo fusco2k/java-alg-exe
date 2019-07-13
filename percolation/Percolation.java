751677eff326a6190865b5a314daa9ae8471ce49
Filipe Fusco <filipe.fusco@gmail.com>
2019-07-13 13:52:25 -0300

    out of bounds fixes

--- percolation/Percolation.java
+++ percolation/Percolation.java
@@ -5,38 +5,74 @@
  **************************************************************************** */
 public class Percolation {
 
-    private int[][] id;
+    private int[][] grid;
+    private int[] id;
     private int size;
 
     // creates n-by-n grid, with all sites initially blocked
     public Percolation(int n) {
-        id = new int[n][n];
+        // initialize the grid array
+        grid = new int[n][n];
+        // initialize the id array
+        id = new int[n * n + 2];
         size = n;
+        // fill the grid with closed spots (0)
         if (n <= 0) {
             throw new IllegalArgumentException("n <= 0");
         }
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                id[i][j] = 0;
+                grid[i][j] = 0;
             }
+        }
+        // fill the id with respective number
+        for (int i = 0; i < n * n + 2; i++) {
+            id[i] = i;
+        }
+        // connect top and bottom
+        for (int i = 1; i <= size; i++) {
+            union(0, i, 0, 0, true, false);
+            union(size, i, 0, 0, false, true);
         }
     }
 
     // opens the site (row, col) if it is not open already
     public void open(int row, int col) {
-        id[row][col] = 1;
+        if (!isOpen(row, col)) {
+            grid[row - 1][col - 1] = 1;
+            if (row - 1 > 0) {
+                if (isOpen(row - 1, col)) {
+                    union(row, col, row - 1, col, false, false);
+                }
+            }
+            if (row != size) {
+                if (isOpen(row + 1, col)) {
+                    union(row, col, row + 1, col, false, false);
+                }
+            }
+            if (col - 1 > 0) {
+                if (isOpen(row, col - 1)) {
+                    union(row, col, row, col - 1, false, false);
+                }
+            }
+            if (col != size) {
+                if (isOpen(row, col + 1)) {
+                    union(row, col, row, col + 1, false, false);
+                }
+            }
+        }
     }
 
-    // is the site (row, col) open?
+    // is the site (row, col) open? normalized
     public boolean isOpen(int row, int col) {
-        return id[row][col] == 1;
+        return grid[row - 1][col - 1] == 1;
     }
 
     // is the site (row, col) full?
     public boolean isFull(int row, int col) {
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
-                if (id[i][j] != 1) {
+                if (grid[i][j] != 1) {
                     return false;
                 }
             }
@@ -49,7 +85,7 @@ public class Percolation {
         int open = 0;
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
-                if (id[i][j] == 1) {
+                if (grid[i][j] == 1) {
                     open++;
                 }
             }
@@ -57,13 +93,35 @@ public class Percolation {
         return open;
     }
 
-    // does the system percolate?
+    // does the system percolate? normalized
     public boolean percolates() {
-        return false;
+        return connected(id[0], id[size + 1]);
+    }
+
+    // normalized
+    public boolean connected(int p, int q) {
+        return id[p] == id[q];
+    }
+
+    public void union(int row, int col, int rol2, int col2, boolean top, boolean bottom) {
+        if (top) {
+            int pid = id[col];
+            int qid = id[0];
+            for (int i = 0; i < id.length; i++)
+                if (id[i] == pid) id[i] = qid;
+        }
+        if (bottom) {
+            int pid = id[col];
+            int qid = id[id.length - 1];
+            for (int i = 0; i < id.length; i++)
+                if (id[i] == pid) id[i] = qid;
+        }
     }
 
     public static void main(String[] args) {
         Percolation perc = new Percolation(2);
-        System.out.println(perc.isFull(2, 2));
+        for (int i = 0; i < perc.size * perc.size + 2; i++) {
+            System.out.println(perc.id[i]);
+        }
     }
 }
